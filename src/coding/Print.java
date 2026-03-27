58b9e1955a5001141ce1d12822b6fae11a785899
Shubham Singh <shubhamsingh7167424@gmail.com>
2026-03-27 18:08:05 +0530

    second commit

---
+++ src/coding/Print.java
@@ -0,0 +1,12 @@
+package coding;
+
+public class Print {
+
+	public static void main(String[] args) {
+		System.out.println("My name is .....");
+		System.out.println("My name is .....");
+		System.out.println("My name is .....");
+		System.out.println("My name is .....");System.out.println("My name is .....");System.out.println("My name is .....");
+	}
+
+}
