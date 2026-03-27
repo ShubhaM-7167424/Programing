14836cb5a54636b95efca571d528567138738b83
Shubham Singh <shubhamsingh7167424@gmail.com>
2026-03-27 18:11:57 +0530

    third commit

---
+++ src/coding/MaxOf2Numbers.java
@@ -0,0 +1,12 @@
+package coding;
+
+public class MaxOf2Numbers {
+
+	public static void main(String[] args) {
+		int a = 10, b=20;
+		
+		int max = a>b ? a: b;		
+		System.out.println(max);
+		
+	}
+}
