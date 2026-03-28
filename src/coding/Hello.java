62d54bc89736286a050e594c75385f1d2fd6c8a0
ShubhaM-7167424 <98887291+ShubhaM-7167424@users.noreply.github.com>
2026-03-28 12:38:34 +0530

    Add additional print statements to Hello.java

--- src/coding/Hello.java
+++ src/coding/Hello.java
@@ -5,5 +5,9 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("I am confident and i am the best");
+
+		System.out.println("today is saturday");
+		System.out.println("tomorrow is sunday");
+		System.out.println("tomorrow is monday");
 	}
 }
