dca44de6f72f0e3f78b870cb27cd5b6e64a9aed2
Shubham Singh <shubhamsingh7167424@gmail.com>
2026-03-28 15:10:46 +0530

    printed

---
+++ src/rahul_folder/Print5.java
@@ -0,0 +1,10 @@
+package rahul_folder;
+
+public class Print5 {
+
+	public static void main(String[] args) {
+		System.out.println("12345");
+		System.out.println("12345");
+		System.out.println("12345");
+	}
+}
